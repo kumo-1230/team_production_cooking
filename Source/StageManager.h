73657123ad3fd5b46f3daef49ab7a8fe7c337853
行雲 橋詰 <yukumo12330@gmail.com>
2025-10-22 19:58:25 +0900

    値段変更

--- Source/StageManager.h
+++ Source/StageManager.h
@@ -64,7 +64,7 @@ private:
 	bool build = true;//ビルドが終わったか
 	int subtractionMoney = 0;//最終的に引かれるお金
 
-	const int ADD_MONEY[3] = { 1000,2000,3000 };
+	const int ADD_MONEY[3] = { 300,500,700 };
 
 	std::unique_ptr<Sprite> sprite;
 	std::unique_ptr<Sprite> spriteWarning;
